c4809cef6f148b67268d60273621bdea2899cc54
Nathaniel Peñaranda <53138333+Nathaniel121215@users.noreply.github.com>
2021-11-15 19:21:52 +0800

    Activity Log Class

---
+++ NCR_SYSTEM_1/ActivityLog_Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCR_SYSTEM_1
+{
+    class ActivityLog_Class
+    {
+        public string Event_ID { get; set; }
+        public string User { get; set; }
+        public string Date_Added { get; set; }
+    }
+}
